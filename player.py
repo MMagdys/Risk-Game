9186619a5da4e28a6dd234d2d57e38168b78f69d
omar gamily <ogamily97@gmail.com>
2019-08-06 21:33:37 +0200

    pacifist ongoing

--- player.py
+++ player.py
@@ -1,25 +1,47 @@
 import heapq
+
+
 class PassiveAgent(object):
+    def __init__(self):
+        self.territories = []
 
-	def __init__(self):
-		self.territories = []
-
-
-
-	def add_army(army, terr):
-
-		armies, name = heapq.heappop(frontier)
-		armies += army
+    def add_army(army, terr):
+        armies, name = heapq.heappop(frontier)
+        armies += army
 
 
 class AggressiveAgent(object):
+    def __init__(self):
+        self.territories = []
 
-	def __init__(self):
-		self.territories = []
+    def add_army(army, terr):
+        armies, name = heapq.heappop(frontier)
+        armies += army
 
 
+class Pacifist:
+    def __init__(self):
+        self.territories = []
 
-	def add_army(army, terr):
+    def add_army(self, terr):
+        terr.add_troops(player=self, troops=3)
 
-		armies, name = heapq.heappop(frontier)
-		armies += army
+    def play(self, opponent):
+        # get all territories for self player
+        # add troops to territory with least troops
+        self.add_army(min(self.territories))
+
+        # get neigbours all territories
+        neighbours = []
+        for i in self.territories:
+            for n in i.neighbours:
+                if n not in neighbours:
+                    neighbours.append(n)
+
+        # check for opponent territories in neigbors and put them in an array
+        attackable =[]
+        for i in neighbours:
+            if i in opponent.territories:
+
+
+    # check for opponent territory with least troops and attack it
--- territory.py
+++ territory.py
@@ -1,38 +1,21 @@
-
 class Territory:
+    def __init__(self, value, neighbours, troops=0, taken_by=None):
+        self.id = value
+        self.neighbours = neighbours
+        self.troops = troops
+        self.taken_by = taken_by
 
+    def __lt__(self, other):
+        return self.troops < other.troops
 
-	def __init__(self, value, neighbours, troops=0, taken_by=None):
+    def __eq__(self, other):
+        return self.troops == other.troops
 
-		self.id = value
-		self.neighbours = neighbours
-		self.troops = troops
-		self.taken_by = taken_by
+    def is_taken(self):
+        if self.taken_by is None:
+            return False
+        return True
 
-
-
-	def __lt__(self, other):
-
-		return self.troops < other.troops
-
-
-
-	def __eq__(self, other):
-
-		return self.troops == other.troops
-
-
-
-	def is_taken(self):
-
-		if self.taken_by is None:
-			return False
-		return True
-
-
-
-	def add_troops(self, player, troops):
-
-		self.taken_by = player
-		self.troops = self.troops + troops
-
+    def add_troops(self, player, troops):
+        self.taken_by = player
+        self.troops = self.troops + troops
