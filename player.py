b692667af23e991c41dea3697f06023f2bfacd1f
ahmedramady <ahmedramady97@gmail.com>
2019-08-06 23:22:14 +0200

    Pacifist agent

--- player.py
+++ player.py
@@ -30,30 +30,21 @@ class AggressiveAgent(object):
 		armies += army
 
 
+class Pacifist:
+    def __init__(self):
+        self.territories = []
 
-# class Pacifist:
-#     def __init__(self):
-#         self.territories = []
+    def play(self, army, opponent):
+        terr = heapq.heappop(self.territories)
+        terr.troops += army
+        heapq.heappush(self.territories, terr)
 
-#     def add_army(self, terr):
-#         terr.add_troops(player=self, troops=3)
+        # get neigbours all territories
+        attack_surface = []
+        for i in self.territories:
+            for n in i.neighbours:
+                if n.taken_by != self:
+                    attack_surface.append((n, i))
 
-#     def play(self, opponent):
-#         # get all territories for self player
-#         # add troops to territory with least troops
-#         self.add_army(min(self.territories))
-
-#         # get neigbours all territories
-#         neighbours = []
-#         for i in self.territories:
-#             for n in i.neighbours:
-#                 if n not in neighbours:
-#                     neighbours.append(n)
-
-#         # check for opponent territories in neigbors and put them in an array
-#         attackable =[]
-#         for i in neighbours:
-#             if i in opponent.territories:
-
-
-#     # check for opponent territory with least troops and attack it
+        attackable_territory = min(attack_surface)
+        if attackable_territory[0].troops < attackable_territory[1].troops - 1:
