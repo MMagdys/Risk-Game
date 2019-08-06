c3218f13253d951804bea238f8ad02dfd8987615
ZizoHussien <zizohussien28@gmail.com>
2019-08-07 01:51:16 +0200

    Aggresive Agent

--- player.py
+++ player.py
@@ -3,31 +3,39 @@ import heapq
 
 class PassiveAgent(object):
 
-	def __init__(self):
-		self.territories = []
+    def __init__(self):
+        self.territories = []
 
 
-	def play(self, army):
+    def play(self, army):
 
-		terr = heapq.heappop(self.territories)
-		# print(terr.troops)
-		terr.troops += army
-		heapq.heappush(self.territories, terr)
-		# print(terr.troops)
+        terr = heapq.heappop(self.territories)
+        # print(terr.troops)
+        terr.troops += army
+        heapq.heappush(self.territories, terr)
+        # print(terr.troops)
 
 
 
 class AggressiveAgent(object):
+    def __init__(self):
 
-	def __init__(self):
-		
-		self.territories = []
+        self.territories = []
 
+    def play(self, army, opponent):
+        terr = max(self.territories)
+        terr.troops += army
+        heapq.heappush(self.territories, terr)
+        # get neigbours all territories
+        attack_surface = []
+        for i in self.territories:
+            for n in i.neighbours:
+                if n.taken_by != self:
+                    attack_surface.append((n, i))
 
-	def play(army, terr):
-
-		armies, name = heapq.heappop()
-		armies += army
+        attackable_territory = max(attack_surface)
+        if attackable_territory[0].troops < attackable_territory[1].troops - 1:
+            attack()
 
 
 class Pacifist:
