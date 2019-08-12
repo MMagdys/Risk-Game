3fda1e3b36b0094b7e99067a534183ce1912f96e
ZizoHussien <zizohussien28@gmail.com>
2019-08-12 23:11:51 +0200

    RealTime A*

--- ai.py
+++ ai.py
@@ -80,6 +80,54 @@ class AstarAgent:
             if attackable_terr[1].troops < attackable_terr[0].troops - 1:
                 attack(attackable_terr[1], attackable_terr[0])
 
+class RealTimeAstarAgent:
+    def __init__(self, board=[], territories=[]):
+        self.board = board
+        self.territories = territories
+    # calculate troops difference
+
+    def calculateHeuristics(self, h):
+        my_goal_terr = []
+        count = 0
+        #h = 0
+        g = 0
+        # h : troops diff
+        # g : number of steps made added to h
+        for terr in self.territories:
+            for n in terr.neighbours:
+                g += 1
+                if n.taken_by != self:
+                    h = (terr.troops - n.troops) + g
+                    g = 0
+            my_goal_terr.append((terr, n, h))
+            h = 0
+            g = 0
+
+        return my_goal_terr
+
+
+    def play(self, army):
+
+        f = []
+        h = 0
+        my_path = []
+        for terr in self.territories:
+            for n in terr.neighbours:
+                if n.taken_by != self:
+                    f.append(self.calculateHeuristics(h))
+                    f.remove(min(f))
+                    h = min(f)
+                    my_path.append(f)
+        count = 1
+        while i in my_path:
+            attackable_terr = my_path[i]
+            if count == 1:
+                attackable_terr[0].troops += army
+                count = 0
+
+            if attackable_terr[1].troops < attackable_terr[0].troops - 1:
+                attack(attackable_terr[1], attackable_terr[0])
+
 
 class Player:
     def __init__(self):
